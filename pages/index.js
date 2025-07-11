dd7dc5875c1532327aef355f330ab01b3a61aa44
Anus-Nancy <personalaccxount@gmail.com>
2025-07-11 18:35:29 +0500

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: "40px", fontFamily: "Arial" }}>
+      <h1>🎉 HumanText Tool is Live!</h1>
+      <p>This is the homepage. You can now start building your humanizer app.</p>
+    </div>
+  );
+}
